3dad91d74d27b5d41a9eb3f58f8eab00e215e725
Afrodinho <54389183+Afrodinho@users.noreply.github.com>
2023-05-08 16:51:08 -0500

    Working Drop down menu

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ Function to generate cost matrix for flights
 Input: none
 Output: Returns a 12 x 4 matrix of prices
 '''
-def get_cost_matrix():
-    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
-    return cost_matrix
+# def get_cost_matrix():
+#     cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+#     return cost_matrix
 
 
 @app.route('/storeData', methods=['POST'])
