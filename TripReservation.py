1c807bee0e788cb19dd30460abb5a9412b8b6492
Afrodinho <54389183+Afrodinho@users.noreply.github.com>
2023-05-07 21:07:57 -0500

    Cost matrix

--- TripReservation.py
+++ TripReservation.py
@@ -22,3 +22,11 @@ def submit_form():
     elif option == 'reserve':
       return redirect('/reserve')
 
+'''
+Function to generate cost matrix for flights
+Input: none
+Output: Returns a 12 x 4 matrix of prices
+'''
+def get_cost_matrix():
+    cost_matrix = [[100, 75, 50, 100] for row in range(12)]
+    return cost_matrix
