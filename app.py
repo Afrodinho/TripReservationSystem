33b7b5c41b0389be4144d6af2e7679d044d227bb
Afrodinho <54389183+Afrodinho@users.noreply.github.com>
2023-05-08 15:23:19 -0500

    Flask now runs webpage

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect,url_for,render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# @app.route('/')
+# def help():
+#     return "HELP ME"
+
+
+@app.route('/', methods=["GET", "POST"])
+def Form():
+    return render_template('form.html')
 
 @app.route('/login')
 def login():
@@ -14,7 +19,7 @@ def login():
 def reserve():
     return render_template('reserve.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def submit_form():
     option = request.form['option']
     if option == 'login':
@@ -22,3 +27,5 @@ def submit_form():
     elif option == 'reserve':
       return redirect('/reserve')
 
+if __name__ == "__app__":
+    app.run(host='0.0.0.0')
